5a252bf87feb87d10667637ffb42695a22173524
Mauricio Velazquez <mauri_i9070@hotmail.com>
2024-09-04 12:24:43 -0300

    Ejercicio completado

--- src/ar/edu/unju/fi/poo/main/Main.java
+++ src/ar/edu/unju/fi/poo/main/Main.java
@@ -27,22 +27,51 @@ public class Main {
                     EmpresaManager.agregarEmpleado(empleados);
                     break;
                 case 2:
-                    System.out.print("Ingrese la antiguedad: ");
-                    int antiguedad = Integer.parseInt(sc.nextLine());
-                    EmpresaManager.mostrarPorAntiguedadMayorA(empleados, antiguedad);
+                    while (true) {
+                        try{
+                            System.out.print("Ingrese la antiguedad: ");
+                            int antiguedad = Integer.parseInt(sc.nextLine());
+                            if (antiguedad < 0){
+                                System.out.println("La antiguedad no puede ser negativa.");
+                            } else {
+                                EmpresaManager.mostrarPorAntiguedadMayorA(empleados, antiguedad);
+                                break;
+                            }
+                        } catch (NumberFormatException e){
+                            System.out.println("Debe ingresar un numero.");
+                        }
+                    }
                     break;
                 case 3:
-                    System.out.print("Ingrese la edad: ");
-                    int edad = Integer.parseInt(sc.nextLine());
-                    EmpresaManager.mostrarPorEdadMayoroIgual(empleados, edad);
+                    while (true) {
+                        try{
+                            System.out.print("Ingrese la edad: ");
+                            int edad = Integer.parseInt(sc.nextLine());
+                            if (verificarEdad(edad)) {
+                                EmpresaManager.mostrarPorEdadMayoroIgual(empleados, edad);
+                                break;
+                            }
+                        } catch (NumberFormatException e){
+                            System.out.println("Debe ingresar un numero.");
+                        }
+                    }
                     break;
                 case 4:
-                    System.out.print("Ingrese la edad: ");
-                    int edadImporte = Integer.parseInt(sc.nextLine());
-                    EmpresaManager.verImporteNetoPorEdadMayoroIgual(empleados, edadImporte);
+                    while (true) {
+                        try{
+                            System.out.print("Ingrese la edad: ");
+                            int edad = Integer.parseInt(sc.nextLine());
+                            if (verificarEdad(edad)) {
+                                EmpresaManager.verImporteNetoPorEdadMayoroIgual(empleados, edad);
+                                break;
+                            }
+                        } catch (NumberFormatException e){
+                            System.out.println("Debe ingresar un numero.");
+                        }
+                    }
                     break;
                 case 5:
-                    int antiguedadAumento = 2;
+                    int antiguedadAumento = 2; // 2 anios
                     EmpresaManager.incrementarSueldoBasicoPorAntiguedad(empleados, antiguedadAumento);
                     break;
                 case 6:
@@ -67,6 +96,15 @@ public class Main {
         System.out.println("5. Incrementar sueldo basico de empleados por antiguedad menor a un valor determinado.");
         System.out.println("6. Ver empleados(todos).");
         System.out.println("7. Salir.");
+    }
+
+    /**
+     * Verifica si la edad es mayor o igual a 18 y menor o igual a 65.
+     * @param edad Edad a verificar.
+     * @return True si la edad es mayor o igual a 18 y menor o igual a 65, false en caso contrario.
+     */
+    public static boolean verificarEdad(int edad){
+        return edad >= 18 && edad <= 65;
     }
 
 }
